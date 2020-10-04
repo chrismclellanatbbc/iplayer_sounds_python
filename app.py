6bb4dcd0a3351c5e5ed4afb67f95c3bdde0f4875
Christopher McLellan <christopher.mclellan@bbc.co.uk>
2020-10-04 12:27:21 +0100

    hooking up json data to web front end

--- app.py
+++ app.py
@@ -2,12 +2,25 @@ from flask import Flask
 from flask import render_template
 from datetime import datetime
 import re
+import urllib.request
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    companyNumber = '08528493'
+    companyNumberSearchUrl = "https://api.opencorporates.com/companies/gb/" + str(companyNumber)
+    webUrl = urllib.request.urlopen(companyNumberSearchUrl)
+    if(webUrl.getcode() == 200):
+        data = webUrl.read()
+        json_data = json.loads(data)
+
+        #return data
+        return render_template("index.html", data=json_data)
+    else:
+        print("Received error, cannot parse results")
+    #return render_template("index.html")
 
 @app.route("/about/")
 def about():
@@ -16,8 +29,6 @@ def about():
 @app.route("/contact/")
 def contact():
     return render_template("contact.html")
-
-
 
 @app.route("/hello")
 @app.route("/hello/<name>")
@@ -35,19 +46,3 @@ def get_data():
     docstring
     """
     return app.send_static_file("data.json")
-    
-    #now = datetime.now()
-    #formatted_now = now.strftime("%A, %d %B, %Y at %X")
-
-    # Filter the name argument to letters only using regular expressions. URL arguments
-    # can contain arbitrary text, so we restrict to safe characters only.
-    
-    #match_object = re.match("[a-zA-Z]+", name)
-
-    #if match_object:
-    #    clean_name = match_object.group(0)
-    #else:
-    #    clean_name = "Friend"
-
-    #content = "Hello there, " + clean_name + "! It's " + formatted_now
-    #return content
